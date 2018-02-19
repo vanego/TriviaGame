a83293a2171ab134ff62d00b213c020cdf3b0eda
Vanessa Gomez <vanessa@vanessas-air.attlocal.net>
2018-02-19 12:09:32 -0800

    timer

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+var interval;
+    var minutes = 1;
+    var seconds = 5;
+    window.onload = function() {
+        countdown('countdown');
+    }
+
+    function countdown(element) {
+        interval = setInterval(function() {
+            var el = document.getElementById(element);
+            if(seconds == 0) {
+                if(minutes == 0) {
+                    el.innerHTML = "countdown's over!";  
+                    alert("countdown's over!");
+                    clearInterval(interval);
+                    return;
+                } else {
+                    minutes--;
+                    seconds = 60;
+                }
+            }
+            if(minutes > 0) {
+                var minute_text = minutes + (minutes > 1 ? ' minutes' : ' minute');
+            } else {
+                var minute_text = '';
+            }
+            var second_text = seconds > 1 ? 'seconds' : 'second';
+            el.innerHTML = minute_text + ' ' + seconds + ' ' + second_text + ' remaining';
+            document.title = minute_text + ' ' + seconds + ' ' + second_text + ' remaining';
+            seconds--;
+        }, 1000);
+    }
